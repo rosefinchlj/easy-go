3295a480547a38d86bf75d0172850323d4fe0096
liujun <380654493@qq.com>
2022-07-18 23:51:52 +0800

    little change

--- log/zap.go
+++ log/zap.go
@@ -1,91 +1,91 @@
 package log
 
 import (
-    "go.uber.org/zap"
-    "os"
+	"go.uber.org/zap"
+	"os"
 )
 
 //
 // todo 后面要做详细配置：日志滚动，日志文件名，日志保留时间...
 //
 var (
-    modeEnv       = "GO_ENV"
-    modeLevelProd = "release"
+	modeEnv       = "GO_ENV"
+	modeLevelProd = "release"
 
-    // log meta:
-    logger *zap.Logger
-    sugar  *zap.SugaredLogger
+	// log meta:
+	logger *zap.Logger
+	sugar  *zap.SugaredLogger
 )
 
 func init() {
-    //
-    mode := os.Getenv(modeEnv)
+	//
+	mode := os.Getenv(modeEnv)
 
-    // 日志级别:
-    if mode == modeLevelProd {
-        logger, _ = zap.NewProduction()
-        defer logger.Sync() // flushes buffer, if any
+	// 日志级别:
+	if mode == modeLevelProd {
+		logger, _ = zap.NewProduction()
+		defer logger.Sync() // flushes buffer, if any
 
-        sugar = logger.Sugar()
-        Infof("log level: %v mode", mode)
-    } else {
-        logger, _ = zap.NewDevelopment()
-        defer logger.Sync() // flushes buffer, if any
+		sugar = logger.Sugar()
+		//Infof("log level: %v mode", mode)
+	} else {
+		logger, _ = zap.NewDevelopment()
+		defer logger.Sync() // flushes buffer, if any
 
-        sugar = logger.Sugar()
-        Info("log level: dev mode")
-    }
+		sugar = logger.Sugar()
+		//Info("log level: dev mode")
+	}
 
-    return
+	return
 }
 
 func Info(args ...interface{}) {
-    sugar.Info(args...)
+	sugar.Info(args...)
 }
 
 // Warn uses fmt.Sprint to construct and log a message.
 func Warn(args ...interface{}) {
-    sugar.Warn(args...)
+	sugar.Warn(args...)
 }
 
 func Debug(args ...interface{}) {
-    sugar.Debug(args...)
+	sugar.Debug(args...)
 }
 
 func Error(args ...interface{}) {
-    sugar.Debug(args...)
+	sugar.Debug(args...)
 }
 
 func Fatal(args ...interface{}) {
-    sugar.Fatal(args...)
+	sugar.Fatal(args...)
 }
 
 func Infof(template string, args ...interface{}) {
-    sugar.Infof(template, args...)
+	sugar.Infof(template, args...)
 }
 
 func Warnf(template string, args ...interface{}) {
-    sugar.Warnf(template, args...)
+	sugar.Warnf(template, args...)
 }
 
 func Debugf(template string, args ...interface{}) {
-    sugar.Debugf(template, args...)
+	sugar.Debugf(template, args...)
 }
 
 func Errorf(template string, args ...interface{}) {
-    sugar.Errorf(template, args...)
+	sugar.Errorf(template, args...)
 }
 
 func FatalF(template string, args ...interface{}) {
-    sugar.Fatalf(template, args...)
+	sugar.Fatalf(template, args...)
 }
 
 func Infow(msg string, keysAndValues ...interface{}) {
-    sugar.Infow(msg, keysAndValues...)
+	sugar.Infow(msg, keysAndValues...)
 }
 
 func Warnw(msg string, keysAndValues ...interface{}) {
-    sugar.Warnw(msg, keysAndValues...)
+	sugar.Warnw(msg, keysAndValues...)
 }
 
 // Debugw logs a message with some additional context. The variadic key-value
@@ -94,9 +94,9 @@ func Warnw(msg string, keysAndValues ...interface{}) {
 // When debug-level logging is disabled, this is much faster than
 //  s.With(keysAndValues).Debug(msg)
 func Debugw(msg string, keysAndValues ...interface{}) {
-    sugar.Debugw(msg, keysAndValues...)
+	sugar.Debugw(msg, keysAndValues...)
 }
 
 func Errorw(msg string, keysAndValues ...interface{}) {
-    sugar.Errorw(msg, keysAndValues...)
+	sugar.Errorw(msg, keysAndValues...)
 }
